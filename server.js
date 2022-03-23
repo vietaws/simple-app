088174e778043435211aac7524939b6d1e0db1e2
Kevin <viet@mail.com>
2022-03-23 18:45:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h2 style="color: orange; text-align:center">VietAWS222<h2>');
+  res.send('<h2 style="color: orange; text-align:center">VietAWS<h2>');
 });
 
 app.listen(PORT, HOST);
