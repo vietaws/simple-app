34660fe61ceb2707d62cde7a15690af6d244f184
Viet <viettq@3c0630014423.ant.amazon.com>
2022-03-27 16:29:54 +0700

    new html

--- server.js
+++ server.js
@@ -9,8 +9,25 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h2 style="color: orange; text-align:center">VietAWS<h2>');
+  res.send(
+    "<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS from $(hostname -f)</h1></body>"
+  );
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+// #!/bin/bash
+// sudo yum update -y
+// sudo yum install -y httpd
+// sudo service httpd start
+// sudo systemctl enable httpd.service
+// echo "<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>VietAWS</h1></body>" > /var/www/html/index.html
+
+// #!/bin/bash
+// # install httpd
+// sudo yum update -y
+// sudo yum install -y httpd
+// sudo service httpd start
+// sudo systemctl enable httpd.service
+// echo "<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS from $(hostname -f)</h1></body>" > /var/www/html/index.html
