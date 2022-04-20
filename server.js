0a21269035ddcb8b6433ae3e41c123ac6c49c7f2
Viet <viettq@3c0630014423.ant.amazon.com>
2022-04-20 22:39:46 +0700

    send to ecr

--- server.js
+++ server.js
@@ -11,24 +11,9 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS from ECR ${os.hostname()}</h1></body>`
   );
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-// #!/bin/bash
-// sudo yum update -y
-// sudo yum install -y httpd
-// sudo service httpd start
-// sudo systemctl enable httpd.service
-// echo "<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>VietAWS</h1></body>" > /var/www/html/index.html
-
-// #!/bin/bash
-// # install httpd
-// sudo yum update -y
-// sudo yum install -y httpd
-// sudo service httpd start
-// sudo systemctl enable httpd.service
-// echo "<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS from $(hostname -f)</h1></body>" > /var/www/html/index.html
