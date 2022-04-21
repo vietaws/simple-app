204ad306631b47a22e44608abde6841b64b24e34
Viet <viettq@3c0630014423.ant.amazon.com>
2022-04-21 11:39:16 +0700

    codepipeline trigger

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS from ECR ${os.hostname()}</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
   );
 });
 
