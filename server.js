6e13d0abe7f56c508bda9d13711091715a06aefa
Viet <viettq@3c0630014423.ant.amazon.com>
2022-04-22 13:03:57 +0700

    update buildspec

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS 111 ${os.hostname()}</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS from ECS ${os.hostname()}</h1></body>`
   );
 });
 
